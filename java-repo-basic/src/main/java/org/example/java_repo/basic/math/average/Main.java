529013ee7d5e1993299dc1559e5ed538b6279c6e
gujun <gujun@qiyi.com>
2020-05-26 11:19:01 +0800

    一组数的最大公约数。

--- java-repo-basic/src/main/java/org/example/java_repo/basic/math/average/Main.java
+++ java-repo-basic/src/main/java/org/example/java_repo/basic/math/average/Main.java
@@ -10,11 +10,11 @@ public class Main {
 
     /**
      * 返回两个或两个以上数字的平均值
-     * @param arr 数组
+     * @param nums 数组
      * @return 平均值
      */
-    public static double average(int[] arr) {
-        return IntStream.of(arr)
+    public static double average(int[] nums) {
+        return IntStream.of(nums)
                 .average()
                 .orElseThrow(()-> new IllegalArgumentException("Array is empty"));
     }
---
+++ java-repo-basic/src/main/java/org/example/java_repo/basic/math/gcd/Main.java
@@ -0,0 +1,22 @@
+package org.example.java_repo.basic.math.gcd;
+
+import com.sun.tools.classfile.ConstantPool;
+
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.OptionalInt;
+
+/**
+ * @author gujun@qiyi.com
+ * @since 2020/5/26 11:11 上午
+ */
+public class Main {
+
+    public static OptionalInt gcd(int[] nums) {
+        return Arrays.stream(nums).reduce(Main::gcd);
+    }
+
+    private static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a%b);
+    }
+}
